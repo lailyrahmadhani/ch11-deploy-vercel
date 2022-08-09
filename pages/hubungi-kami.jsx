b17c9335a5a48be472b1ddfb949279a8f20ed488
Wahyu Budi Saputra <wsaputra@binaracademy.org>
2022-08-09 18:00:46 +0800

    Implement list of article

---
+++ pages/hubungi-kami.jsx
@@ -0,0 +1,9 @@
+import { Container } from "react-bootstrap";
+
+export default function AboutUs() {
+  return (
+    <Container>
+      <h1>Hubungi Kami</h1>
+    </Container>
+  );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,32 @@
-export default function Home() {
-  return <></>;
+import Head from "next/head";
+import Link from "next/link";
+import { Card, Row, Col, Container } from "react-bootstrap";
+
+export async function getServerSideProps() {
+  const res = await fetch("http://localhost:4000/articles");
+  const articles = await res.json();
+
+  return { props: { articles } };
+}
+
+export default function Home({ articles }) {
+  return (
+    <Container className="mt-3">
+      <Head>
+        <title>List of Article</title>
+      </Head>
+
+      <Row className="g-3">
+        {articles.map((article) => (
+          <Col lg={4} key={article.id}>
+            <Card body>
+              <Link href={`/article/${article.id}`}>
+                <a>{article.title}</a>
+              </Link>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
 }
